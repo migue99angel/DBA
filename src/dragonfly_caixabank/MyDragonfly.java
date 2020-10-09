7dd78a19e070a138ff97ba5bf78b079e0917b9b4
mumo <miguemumo99@correo.ugr.es>
2020-10-09 12:12:10 +0200

    Creado el proyecto de Netbeans

---
+++ src/dragonfly_caixabank/MyDragonfly.java
@@ -0,0 +1,12 @@
+package dragonfly_caixabank;
+
+import IntegratedAgent.IntegratedAgent;
+
+public class MyDragonfly extends IntegratedAgent{
+    
+    @Override
+    public void plainExecute() {
+        
+    }
+    
+}
