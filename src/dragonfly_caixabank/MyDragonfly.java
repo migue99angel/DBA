fa023062bad1f4144a31475ff9b60f36d90244fd
Miguel Angel <posadasarraez@gmail.com>
2020-10-25 18:46:57 +0100

    Avanzando en la P2

--- src/dragonfly_caixabank/MyDragonfly.java
+++ src/dragonfly_caixabank/MyDragonfly.java
@@ -1,5 +1,6 @@
 package dragonfly_caixabank;
 
+import ControlPanel.TTYControlPanel;
 import IntegratedAgent.IntegratedAgent;
 import com.eclipsesource.json.Json;
 import com.eclipsesource.json.JsonArray;
@@ -11,9 +12,12 @@ import java.io.IOException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
+
+
 public class MyDragonfly extends IntegratedAgent{
-    
+    TTYControlPanel myControlPanel;
     String receiver;
+    int alive;
     @Override
     public void setup() {
         super.setup();
@@ -21,73 +25,115 @@ public class MyDragonfly extends IntegratedAgent{
         doCheckinPlatform();
         doCheckinLARVA();
         receiver = this.whoLarvaAgent();
-        
+        myControlPanel = new TTYControlPanel(getAID());
         _exitRequested = false;
     }
 
     @Override
     public void plainExecute() {
         
+        ACLMessage in;
         ACLMessage out = new ACLMessage();
+        boolean onTarget = false;
         out.setSender(getAID());
         out.addReceiver(new AID(receiver,AID.ISLOCALNAME));
         
+        
         //Hacemos el login
-        JsonObject obj = new JsonObject();
-        obj.add("command", "login");
-        obj.add("world", "BasePlayground");
-        String mysensors[] = {"alive","gps"};
+        JsonObject jsonObjIn = new JsonObject();
+        jsonObjIn.add("command", "login");
+        jsonObjIn.add("world", "Playground1");
+
         JsonArray toContent = new JsonArray();
         toContent.add("alive");
         toContent.add("gps");
-        obj.add("attach", toContent);
+        toContent.add("ontarget");
+        toContent.add("altimeter");
+        toContent.add("compass");
+        toContent.add("visual");
+        toContent.add("lidar");
+       
+        jsonObjIn.add("attach", toContent);
         
-        out.setContent(obj.toString());
+        out.setContent(jsonObjIn.toString());
         this.sendServer(out);
         
         //Recibimos la key
-        ACLMessage in = this.blockingReceive();
+        in = this.blockingReceive();
         Info("Respuesta");
         Info(in.getContent());
-        JsonObject jsonObj = new JsonObject();
-        jsonObj = Json.parse(in.getContent().toString()).asObject();
-
+        JsonObject jsonObjOut = new JsonObject();
+        jsonObjOut = Json.parse(in.getContent().toString()).asObject();
+        
+        int width = jsonObjOut.get("width").asInt();
+        int height = jsonObjOut.get("height").asInt();
+        int maxheight = jsonObjOut.get("maxflight").asInt();
+        String key = jsonObjOut.get("key").asString();
+        
         //Hacemos la lectura de los sensores
-        obj = new JsonObject();
-        obj.add("command", "read");
-        obj.add("key", jsonObj.get("key"));
-        
-        out.setContent(obj.toString());
+        jsonObjIn = new JsonObject();
+        jsonObjIn.add("command", "read");
+        jsonObjIn.add("key", key);
+
+        out = in.createReply();
+        out.setContent(jsonObjIn.toString());
         this.sendServer(out);
-        
+
         //Recibimos los valores de los sensores
-        ACLMessage in2 = this.blockingReceive();
-        Info("Lectura");
-        Info(in2.getContent());
-        
-        //Ejecutamos una acción
-        obj = new JsonObject();
-        obj.add("command", "execute");
-        obj.add("action", "rotateR");
-        obj.add("key", jsonObj.get("key"));
-        
-        ACLMessage out2 = in2.createReply();
-        out2.setContent(obj.toString());
-        this.sendServer(out2);
-        
-        //Recibimos respuesta de la ejecución de la acción
-        ACLMessage in3 = this.blockingReceive();
-        Info("Ejecución");
-        Info(in3.getContent());
-        
+        in = this.blockingReceive();
+        jsonObjOut = Json.parse(in.getContent().toString()).asObject();
+        lecturaSensores(jsonObjOut);
+        //while(!onTarget && this.alive == 1)
+        for(int i = 0; i < 10; i++)
+        {
+            System.out.println(this.alive);
+            
+            myControlPanel.feedData(in,width , height, maxheight);
+            myControlPanel.fancyShow();
+            
+            Info("Lectura");
+            Info(in.getContent());
+
+            //Ejecutamos una acción
+            jsonObjIn = new JsonObject();
+            jsonObjIn.add("command", "execute");
+     
+            jsonObjIn.add("action", "rotateL");
+            jsonObjIn.add("key", key);
+
+            out = in.createReply();
+            out.setContent(jsonObjIn.toString());
+            
+            this.sendServer(out);
+            
+            //Recibimos respuesta de la ejecución de la acción
+            in = this.blockingReceive();
+            Info("Ejecución");
+            Info(in.getContent());
+            
+            //Hacemos la lectura de los sensores
+            jsonObjIn = new JsonObject();
+            jsonObjIn.add("command", "read");
+            jsonObjIn.add("key", key);
+
+            out = in.createReply();
+            out.setContent(jsonObjIn.toString());
+            this.sendServer(out);
+
+            //Recibimos los valores de los sensores
+            in = this.blockingReceive();
+            jsonObjOut = Json.parse(in.getContent().toString()).asObject();
+            lecturaSensores(jsonObjOut);
+        }
+        myControlPanel.close();
         //Logout
-        obj = new JsonObject();
-        obj.add("command", "logout");
-        obj.add("key", jsonObj.get("key"));
+        jsonObjIn = new JsonObject();
+        jsonObjIn.add("command", "logout");
+        jsonObjIn.add("key", key);
         
-        ACLMessage out3 = in3.createReply();
-        out3.setContent(obj.toString());
-        this.sendServer(out3);
+        out = in.createReply();
+        out.setContent(jsonObjIn.toString());
+        this.sendServer(out);
 
         _exitRequested = true;
     }
@@ -100,7 +146,22 @@ public class MyDragonfly extends IntegratedAgent{
     }
 
     protected String whoLarvaAgent() {
-        return "WorldManager";
+        return "CaixaBank";
     }
     
+    void lecturaSensores(JsonObject jsonObjOut)
+    {
+        JsonArray aux = jsonObjOut.get("details").asObject().get("perceptions").asArray();
+        
+        for(int i = 0; i < aux.size(); i++ )
+        {
+            switch(aux.get(i).asObject().get("sensor").asString())
+            {
+                case "alive":
+                   this.alive = aux.get(i).asObject().get("data").asArray().get(0).asInt();
+                   break;
+            }
+        }
+        
+    }
 }
